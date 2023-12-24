3c9584ddd84b074ca6fb9d79fdfa2fa6d66b0d91
astasiempre <ero1494651@gmail.com>
2023-12-24 21:37:32 +0200

    first commit

--- controllers/auth/index.js
+++ controllers/auth/index.js
@@ -3,10 +3,12 @@ const { register } = require('./auth');
 const { login } = require('./auth');
 const { getCurrent } = require('./auth');
 const { logout } = require('./auth');
+const {updateAvatar} = require('./auth');
 
 module.exports = {
     register,
     login,
     getCurrent,
     logout,
+updateAvatar,
 };
--- middlewares/index.js
+++ middlewares/index.js
@@ -3,8 +3,11 @@ const validateBody = require("./validateBody");
 const isValidId = require("./isValidId");
 
 const authenticate = require("./authenticate");
+const upload = require("./upload")
+
 module.exports = {
     validateBody,
     isValidId,
     authenticate,
+    upload,
 }
