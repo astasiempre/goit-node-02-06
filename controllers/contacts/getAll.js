eb0fc51f34156171d28e5df303a9a9191486ea67
astasiempre <ero1494651@gmail.com>
2024-01-05 22:34:07 +0200

    docker

--- controllers/contacts/getAll.js
+++ controllers/contacts/getAll.js
@@ -1,13 +1,7 @@
 const { Contact } = require("../../models/contact");
 const { HttpError, ctrlWrapper } = require("../../helpers");
 
-// const getAll = async (req, res) => {
-//   const { _id: owner } = req.user;
-//   // const { page = 1, limit = 5 } = req.query;
-//   // const skip = (page - 1) * limit;
-//   const result = await Contact.find({ owner }).populate( "owner");
-//   res.json(result);
-// };
+
 const getAll = async (req, res) => {
     const {_id: owner} = req.user;
     const {page = 1, limit = 10} = req.query;
